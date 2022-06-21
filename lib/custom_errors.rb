757502cb4559d1ffcfec4f5c19a6984e6d2662dc
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-06-21 10:28:40 -0500

    commit tree b1108abc5e59715989b84aed83c1a05268ce7e69

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Person
   attr_accessor :partner, :name
 
@@ -6,16 +7,36 @@ class Person
   end
 
   def get_married(person)
+
+    if person.is_a?(Person)
     self.partner = person
     person.partner = self
+    else 
+      begin
+      raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    end
+  end
+
+  class PartnerError < StandardError
+    def message
+      'gotta be a Person'
+    end
   end
 
 end
+
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
 
+# class Fish < Person
+# end
 
 
 
+
+binding.pry
